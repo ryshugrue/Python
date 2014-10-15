0fb7992d2e36c8fb02dae78941758a1d47d5e1e7
ryshugrue <shugrue.ryan@gmail.com>
2014-10-15 16:12:28 -0500

    Homework day 1

---
+++ homework-1.py
@@ -0,0 +1,19 @@
+import math
+
+first = "Ryan"
+last = "Shugrue"
+age = 14
+print first, last, age
+
+sideA = 12.55
+sideB = 17.85
+sideC = math.sqrt( (sideA * sideA) + (sideB * sideB))
+print sideA, sideB, sideC
+
+
+
+operand1 = 95
+operand2 = 64.5
+
+print "95 + 64.5 = ", 95 + 64.5, "\n95 - 64.5 = ", 95 - 64.5, "\n95 * 64.5 = ", 95 * 64.5, "\n95 / 64.5 = ", 95 / 64.5, "\n95 % 64.5 = ", 95 % 64.5
+
